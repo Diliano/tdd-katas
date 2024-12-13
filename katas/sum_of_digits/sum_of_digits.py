b85f836a366e1a1f7d6effb02a461ecdc1e84c04
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-12-13 18:05:13 +0000

    [sum of digits] add a docstring outlining the implementation approach

--- katas/sum_of_digits/sum_of_digits.py
+++ katas/sum_of_digits/sum_of_digits.py
@@ -1,4 +1,14 @@
 def sum_of_digits(num):
+    """
+    Calculates the sum of digits in a non-negative integer
+
+    Approach:
+    1) If the number has only one digit, return it
+    2) Otherwise:
+        - repeatedly extract the last digit using `num % 10`
+        - add the extracted digit to a running total
+        - remove the extracted digit using floor division `num //= 10`
+    """
     if num < 10:
         return num
 
