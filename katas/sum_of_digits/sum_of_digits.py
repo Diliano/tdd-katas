ea666f3ea6deb69aa7a1607d68f31b6cc3f014b2
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-12-13 18:00:37 +0000

    [sum of digits] refactor nomenclature to use digit_sum rather than sum, distinguishing from the built in function

--- katas/sum_of_digits/sum_of_digits.py
+++ katas/sum_of_digits/sum_of_digits.py
@@ -2,10 +2,10 @@ def sum_of_digits(num):
     if num < 10:
         return num
 
-    sum = 0
+    digit_sum = 0
 
     while num != 0:
-        sum += num % 10
+        digit_sum += num % 10
         num //= 10
 
-    return sum
+    return digit_sum
