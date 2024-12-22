3922195a62ab6a0cccc744a66285850341c08fee
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-12-22 13:11:10 +0000

    [move zeroes] refactor for readability

--- katas/move_zeroes/move_zeroes.py
+++ katas/move_zeroes/move_zeroes.py
@@ -1,11 +1,8 @@
 def move_zeroes(nums):
-    if not nums:
-        return []
-
     set_nums = set(nums)
+    empty = not nums
+    no_zeroes = 0 not in set_nums
+    only_zeroes = len(set_nums) == 1 and set_nums == {0}
 
-    if 0 not in set_nums:
-        return nums
-
-    if len(set_nums) == 1 and set_nums == {0}:
+    if empty or no_zeroes or only_zeroes:
         return nums
