30b0a82718e09b19b02a4c5ae1296e4d08868165
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-12-23 21:02:57 +0000

    [move zeroes] add a docstring to outline the implementation approach

--- katas/move_zeroes/move_zeroes.py
+++ katas/move_zeroes/move_zeroes.py
@@ -1,4 +1,15 @@
 def move_zeroes(nums):
+    """
+    Moves all zeroes to the end of the list
+
+    Approach:
+    1) Use a pointer to track the position for the next non-zero
+        - Starting at index 0
+    2) Iterate through the list
+        - If a zero is found, skip it
+        - If a non-zero is found, swap it with the element at the pointer's position
+    3) Increment the pointer after each swap
+    """
     j = 0
 
     for i in range(len(nums)):
