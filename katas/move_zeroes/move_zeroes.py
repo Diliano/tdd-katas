1fa8241464602c7913150f62e76b11c85c75dca1
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-12-23 20:59:01 +0000

    [move zeroes] refactor to use position swapping, moving all non-zeroes to the left and all zeroes to the right of the list

--- katas/move_zeroes/move_zeroes.py
+++ katas/move_zeroes/move_zeroes.py
@@ -1,17 +1,9 @@
 def move_zeroes(nums):
-    set_nums = set(nums)
-    empty = not nums
-    no_zeroes = 0 not in set_nums
-    only_zeroes = len(set_nums) == 1 and set_nums == {0}
+    j = 0
 
-    if empty or no_zeroes or only_zeroes:
-        return nums
-
-    length = len(nums)
-
-    for i in range(length):
-        if nums[i] == 0:
-            nums.pop(i)
-            nums.append(0)
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[i], nums[j] = nums[j], nums[i]
+            j += 1
 
     return nums
