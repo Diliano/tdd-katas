de6c6c72604143d670955e3d75e41e6d3c8906a5
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-12-26 18:00:26 +0000

    [first non-repeating character] add a docstring to outline the implementation approach

--- katas/first_non_repeating_character/first_non_repeating_character.py
+++ katas/first_non_repeating_character/first_non_repeating_character.py
@@ -2,6 +2,18 @@ from collections import Counter
 
 
 def first_non_repeating_character(s):
+    """
+    Returns the first non-repeating character in a string
+
+    Approach:
+    1) If the string is empty or has only one character:
+        - Return the string
+    2) Use `Counter` to count occurences of each character
+    3) Iterate through the string:
+        - Return the first character with a count of 1
+    4) If a non-repeating character is not found:
+        - Return an empty string
+    """
     if not s or len(s) == 1:
         return s
 
